843f3b64fe805806fd59bf3f623914bb98f5217a
Ramkumar Natarajan <nrkumar93@gmail.com>
2020-03-05 17:59:59 -0500

    adjusted common and viz

--- ocrl/scripts/common.py
+++ ocrl/scripts/common.py
@@ -2,6 +2,13 @@
 
 import numpy as np
 
+import rospy
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Point, PoseArray
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+
 x_lim = [-10, 10]
 y_lim = [-10, 10]
 theta_lim = [-np.pi, np.pi]
--- ocrl/scripts/viz.py
+++ ocrl/scripts/viz.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-import numpy as np
-
-import rospy
-from tf.transformations import quaternion_from_euler, euler_from_quaternion
-from geometry_msgs.msg import Quaternion
-from geometry_msgs.msg import Point, PoseArray
-from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray
 
 from .common import *
 
